871b78c5af9419251fc97dc5842a58279db8e27d
Sev Sunday <823864+sevsunday@users.noreply.github.com>
2024-07-13 22:48:01 -0500

    Proxy confirmed
    Proxy confirmed needed, cleaned up README

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-Simple website to pull multi-player lobby data from Battlezone II.
+# Battlezone II Lobby Watch
+
+Simple website to pull real-time multi-player lobby data from Battlezone II.
 
 Relies on data from: http://multiplayersessionlist.iondriver.com/
 
-Uses a free CORS Proxy to get around the AJAX problems: 
+Uses a proxy to get around the CORS policy issue: 
 
 https://codetabs.com/cors-proxy/cors-proxy.html 
 https://api.codetabs.com/v1/proxy?quest=URL
+
+NOTE: proxy limited to 5 requests per second. 
--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 async function getLobbyData() {
     
+    // Requesting data directly gets blocked by CORS policy, so we use klugey CORS Proxy workaround
     const sourceURL = "http://multiplayersessionlist.iondriver.com/api/1.0/sessions?game=bigboat:battlezone_combat_commander";
     const proxyURL = 'https://api.codetabs.com/v1/proxy/?quest=' + sourceURL;
 
     try {
-        let fetchResponse = await fetch(sourceURL);
+        let fetchResponse = await fetch(proxyURL);
 
         if( !fetchResponse.ok ) {
             console.log(`Error with response. Make sure source and proxy URLs are accessible and returning valid data.`);
