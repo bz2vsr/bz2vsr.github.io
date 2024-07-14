25435b2943ea83b7cafb70be8a0d596fadac1f5b
Sev Sunday <823864+sevsunday@users.noreply.github.com>
2024-07-13 19:13:02 -0500

    test data pull

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
+async function getLobbyData() {
+    
+    let dataURL = "http://multiplayersessionlist.iondriver.com/api/1.0/sessions?game=bigboat:battlezone_combat_commander";
+
+    try {
+        let rawData = await fetch(dataURL);
+
+        if( !rawData.ok ) {
+            console.log(`Error with data. Make sure ${dataURL} is accessible and returning valid data.`);
+        }
+
+        let data = await rawData.json();
+
+        console.log(data);
+
+    } catch {
+
+        console.log(`Catch: Make sure ${dataURL} is accessible and returning valid data.`);
+
+    }
+}
 window.addEventListener('DOMContentLoaded', (event) => {
 
-    alert('foobar');
+    getLobbyData();
 
-});
+})
