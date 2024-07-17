6da598487789e12c0b5defa70f13d003a428c0e5
Sev Sunday <823864+sevsunday@users.noreply.github.com>
2024-07-16 19:01:21 -0500

    add better mobile support

--- js/main.js
+++ js/main.js
@@ -167,7 +167,7 @@ async function getLobbyData() {
                                     else if( (PlayerList[player].Team !== undefined) && PlayerList[player].Team.Leader === true ) {
                                         return `<div class="col-6 player-slot p-2">
                                             <div class="d-block p-2 fw-bold bg-primary border border-dark bg-gradient bg-opacity-50 rounded ps-3 d-flex justify-content-between align-items-center" style="--bs-border-opacity: .25;">
-                                                <span>
+                                                <span class="text-nowrap overflow-hidden">
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill mb-1" viewBox="0 0 16 16">
                                                     <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
                                                     </svg>
@@ -184,7 +184,7 @@ async function getLobbyData() {
                                     else {
                                         return `<div class="col-6 player-slot p-2">
                                             <div class="d-block p-2 fw-bold bg-primary border border-dark bg-gradient bg-opacity-50 rounded ps-3 d-flex justify-content-between align-items-center" style="--bs-border-opacity: .25;">
-                                                <span>
+                                                <span class="text-nowrap overflow-hidden">
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill mb-1" viewBox="0 0 16 16">
                                                     <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
                                                     </svg>
