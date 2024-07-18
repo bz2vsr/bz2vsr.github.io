2f3af96427677a4e5e5ca01598407d5e2987dcb3
Sev Sunday <823864+sevsunday@users.noreply.github.com>
2024-07-17 20:30:28 -0500

    minor mobile improvement
    for full view only

--- js/full.js
+++ js/full.js
@@ -241,11 +241,11 @@ async function getLobbyData() {
                                                     <a href="${Steam.ProfileUrl}" target="_blank" class="text-decoration-none text-light">
                                                         <div class="d-block p-2 bg-primary border border-dark bg-gradient bg-opacity-50 rounded ps-3" style="--bs-border-opacity: .25;">
                                                             <div class="row">
-                                                                <div class="col-3">
+                                                                <div class="col-3 d-none d-lg-inline"">
                                                                     <img src="${Steam.AvatarUrl}" onError="this.src='../img/no_steam_pfp.jpg'" class="img-fluid img-thumbnail rounded"/>
                                                                 </div>
-                                                                <div class="col-9">
-                                                                    <span class="text-nowrap overflow-hidden small font-monospace">
+                                                                <div class="col-9 text-nowrap overflow-hidden">
+                                                                    <span class="small font-monospace">
                                                                         Nick: ${truncate(clean(PlayerList[player].Name), 24)}<br>
                                                                         Steam: ${truncate(clean(Steam.Nickname), 24)}<br>
                                                                         ${(() => {
