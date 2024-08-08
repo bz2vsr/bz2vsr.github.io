e4bf388cb8211028db643c77494c6810f88e1dae
Sev Sunday <823864+sevsunday@users.noreply.github.com>
2024-08-08 03:36:55 -0500

    2nd attempt at adding short URL generation

--- js/full.js
+++ js/full.js
@@ -185,17 +185,19 @@ async function getLobbyData() {
                     headers: {
                         accept: 'application/json',
                         'content-type': 'application/json',
-                        Authorization: 'sk_7BCBrO9UB4kbuQRQ'
+                        Authorization: 'pk_gj3EhDNijDovvGvU'
                     },
-                    body: JSON.stringify({
-                        originalURL: directJoinURL,
-                        domain: 'go.bz2vsr.com'
-                    })
+                    body: JSON.stringify({ 
+                        domain: 'join.bz2vsr.com', 
+                        originalURL: 'https://bz2vsr.com/?join=' + encodedArgs, 
+                        path: game.Address.NAT })
                 };
 
-                fetch('https://api.short.io/links', options)
+                fetch('https://api.short.io/links/public', options)
                     .then(response => response.json())
-                    .then(response => console.log(response))
+                    .then(response => { 
+                        document.querySelector('#shareJoinURL').innerHTML = response.shortURL;
+                    })
                     .catch(err => console.error(err));
 
             }
@@ -280,7 +282,7 @@ async function getLobbyData() {
                                                     </svg>
                                                 </a> 
                                                 <button data-join-string='${encodedArgs}' class="btn btn-sm btn-purple btn-join-copy" title="Get a shareable link for Discord.">
-                                                    <textarea class="visually-hidden">${window.location.href + "?join=" + encodedArgs}</textarea>
+                                                    <textarea id="shareJoinURL" class="visually-hidden"></textarea>
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">
                                                         <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z"/>
                                                         <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0z"/>
