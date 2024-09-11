dd324bca5f78f9ea6d694af36839b22143a34067
Sev Sunday <823864+sevsunday@users.noreply.github.com>
2024-09-11 18:55:36 -0500

    make mod names link to mod steam page

--- js/full.js
+++ js/full.js
@@ -507,7 +507,13 @@ async function getLobbyData() {
                         </div>
                         <!-- Card Footer -->
                         <div class="card-footer d-flex justify-content-between align-items-center small">
-                            <span class="btn btn-sm text-secondary border border-secondary btn-dead ps-0 font-monospace" style="--bs-border-opacity: 0;">${gameModName}</span>
+                            <a href="https://steamcommunity.com/sharedfiles/filedetails/?id=${gameMod}" target="_blank" class="link-secondary link-underline-dark border border-secondary ps-0 font-monospace" style="--bs-border-opacity: 0;">
+                                ${gameModName}
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-link-45deg" viewBox="0 0 16 16">
+                                <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1 1 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4 4 0 0 1-.128-1.287z"/>
+                                <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243z"/>
+                                </svg>
+                            </a>
                             <div class="d-flex justify-content-end align-items-center">
                                 ${(() => {
                                     // IIFEs need a return value, otherwise it returns undefined, thus the else statement
