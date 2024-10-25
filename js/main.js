9b940092103e20d980a3598fb31b041ccbd78d8e
Sev Sunday <823864+sevsunday@users.noreply.github.com>
2024-10-25 16:22:33 -0500

    keep the copy-join-url button on mobile
    hides the join button, but keeps the copy-join-url button in case someone wants to share join link on discord from their phone

--- js/main.js
+++ js/main.js
@@ -520,9 +520,9 @@ async function getLobbyData()
                                 ${(() => {
                                     if( hasJoinURL ) {
                                         return `
-                                        <span class="d-none d-lg-inline">
+                                        <span class="">
                                             <div class="btn-group">
-                                                <a href="${directJoinURL}" class="btn btn-sm btn-purple bg-gradient me-1" title="Join the game directly with Steam.">
+                                                <a href="${directJoinURL}" class="d-none d-md-inline btn btn-sm btn-purple bg-gradient me-1" title="Join the game directly with Steam.">
                                                     Join
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-fill mb-1" viewBox="0 0 16 16">
                                                     <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"/>
