28ad78387fe0da7ef3bc36162b919dc9271e6f4f
Sev Sunday <823864+sevsunday@users.noreply.github.com>
2024-10-30 19:41:45 -0500

    Add Discord URI redirect for Copy-Join-URL
    also remove console.log image test

--- js/main.js
+++ js/main.js
@@ -94,20 +94,6 @@ function clean(str)
     return cleaned;
 }
 
-console.image = function(url, size = 100) {
-  const image = new Image();
-  image.src = url;
-  image.onload = function() {
-    var style = [
-      'font-size: 1px;',
-      'padding: ' + this.height/100*size + 'px ' + this.width/100*size + 'px;',
-      'background: url('+ url +') no-repeat;',
-      'background-size: contain;'
-     ].join(' ');
-     console.log('%c ', style);
-  };
-};
-
 // grabs 3 random maps when loading the Map Picker modal
 async function getRandomMaps() {
     // Ensure we have at least 3 unique random indexes
@@ -852,15 +838,16 @@ async function getLobbyData()
                     btn.classList.add('btn-success');
                     
                     // change clipboard icon to a checkmark
-                    btn.innerHTML = `
+                    btn.innerHTML = `${joinStr.outerHTML}
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
                         <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
                         </svg>
                     `;
 
+                    window.location.replace('discord:///channels/1137584338310017098/1137619558639874129');
                     // return copy button to original state after a few seconds
                     setTimeout(function(){
-                        btn.innerHTML = `
+                        btn.innerHTML = `${joinStr.outerHTML}
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">
                                 <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z"/>
                                 <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0z"/>
@@ -882,9 +869,6 @@ async function getLobbyData()
 /*-------------------------------------------------*/
 
 window.addEventListener('DOMContentLoaded', (event) => {
-
-    // const url = '';
-    // console.image(url, 50);
 
     // toggle the "VSR Only" switch based on localStorage value
     if( localStorage.getItem("ShowVSROnly") === "false" ) {
