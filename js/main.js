833093ee56329aba9547dcbcd57ccd33480548eb
Sev Sunday <823864+sevsunday@users.noreply.github.com>
2025-01-09 11:52:31 -0600

    fix linked map

--- js/main.js
+++ js/main.js
@@ -301,10 +301,11 @@ async function getLobbyData()
             let linkedPlayerCards   = localStorage.getItem("LinkedPlayerCards") === "true" ? true : false;
             let compactPlayerCards  = (localStorage.getItem("CompactPlayerCards") === "true" || document.querySelector("#CompactCardsToggle").checked ? true : false);
 
-            // soft test of showing VSR map data for BZ2 Vet Strat game cards
-            let mapVSRObject    = MapData.find(map => map.File.toLowerCase() === mapFileName);
+            // get matching map object from vsrmaplist.json
+            let mapVSRObject    = MapData.find(map => map.File === mapFileName);
 
-
+            console.log(mapVSRObject);
+            console.log(mapFileName);
 
             // if vsr-only is toggled, this exits the current iteration if it isn't VSR
             if( localStorage.getItem("ShowVSROnly") === "true" || document.querySelector("#VSRToggle").checked ) {
@@ -576,15 +577,15 @@ async function getLobbyData()
                                                 data-bs-map-description="${mapVSRObject.Description}" 
                                                 data-bs-map-author="${mapVSRObject.Author}"
                                                 data-bs-map-tags="${mapVSRObject.Tags}"
+                                                />
+                                                <span class="position-absolute top-0 end-0 px-1 text-secondary rounded m-2 vsr-cursor-pointer">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+                                                        <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"/>
+                                                        <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z"/>
+                                                    </svg>
+                                                </span>
                                                 ` 
-                                            : '"'}
-                                        />
-                                          <span class="position-absolute top-0 end-0 px-1 text-secondary rounded m-2 vsr-cursor-pointer">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
-                                                <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"/>
-                                                <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z"/>
-                                            </svg>
-                                          </span>
+                                            : '"/>'}
                                         ${(() => {
                                             if( mapVSRObject && isVetStrat ) {
                                                 return `
