16241e56234edad8f7229ee8cf58401026c0cadb
Sev Sunday <823864+sevsunday@users.noreply.github.com>
2025-01-19 01:12:13 -0600

    Update vid.js

--- js/vid.js
+++ js/vid.js
@@ -165,7 +165,7 @@ function updateEmbeddedPlayer(videoId) {
             <iframe 
                 width="100%" 
                 height="100%" 
-                src="https://www.youtube.com/embed/${videoId}?autoplay=1" 
+                src="https://www.youtube.com/embed/${videoId}" 
                 title="YouTube video player" 
                 frameborder="0" 
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
